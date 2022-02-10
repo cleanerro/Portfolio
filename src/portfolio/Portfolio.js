0848598f90db0087ff3c61c8292bfb230497eeb3
cleanerro <romastepanyuk@gmail.com>
2022-02-11 00:26:33 +0200

    added layout Portfolio ++

--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import s from './Portfolio.module.css';
-import {Project} from "./project/Project";
-import {Title} from "../common/components/title/Title";
+import s from './Portfolio.module.scss';
+import {Project} from './project/Project';
+import {Title} from '../common/components/title/Title';
+import  socialNetworkImg from './../assets/img/sicialNetwork.png'
+import  todoListImg from './../assets/img/todolist.jpg'
 
 export const Portfolio =() =>  {
+
+    const social = {
+        backgroundImage: `url(${socialNetworkImg})`
+    }
+    const todoList = {
+        backgroundImage: `url(${todoListImg})`
+    }
 
     return (
         <div className={s.portfolioBlock}>
@@ -11,8 +20,8 @@ export const Portfolio =() =>  {
                 <Title text = 'portfolio' />
 
                 <div className={s.projects}>
-                  <Project title = {'Social network'} description = {'Ipsum the printing and typesetting industry sum the printing and typesetting industry'}/>
-                  <Project title = {'Todo list'} description = {'Lorem Ipsum is simply printing and '}/>
+                  <Project style = {social} title = {'Social network'} description = {'Ipsum the printing and typesetting industry sum the printing and typesetting industry'}/>
+                  <Project style = {todoList} title = {'Todo list'} description = {'Lorem Ipsum is simply printing and '}/>
 
                 </div>
 
--- src/portfolio/project/Project.js
+++ src/portfolio/project/Project.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import s from './Project.module.css';
+import s from './Project.module.scss';
 
 export const Project =(props) =>  {
     return (
         <div className={s.project}>
-            <div className={s.image}>
-                <button>Open</button>
+            <div className={s.image} style={props.style}>
+                <button className={s.projectBtn}>Open</button>
             </div>
             <h3 className={s.projectTitle}>{props.title}</h3>
             <span className={s.description}>{props.description}</span>
